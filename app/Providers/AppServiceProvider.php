81ef2f6d6ca96ac114f6b84dddc056d0d9d3f922
Rohan Khatri <khatrirohan80@gmail.com>
2024-10-02 18:35:33 +0530

    Dusk Macros for copy/paste

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,16 @@
 
 namespace App\Providers;
 
+use Facebook\WebDriver\WebDriverKeys;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Dusk\Keyboard;
+use Laravel\Dusk\OperatingSystem;
 
+/**
+ * @method type(array $array)
+ * @method selectAll()
+ * @method copy()
+ */
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -19,6 +27,21 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Keyboard::macro('selectAll', function () {
+            $this->type([OperatingSystem::onMac() ? WebDriverKeys::COMMAND : WebDriverKeys::CONTROL, 'a']);
+        });
+
+        Keyboard::macro('copy', function () {
+            $this->type([OperatingSystem::onMac() ? WebDriverKeys::COMMAND : WebDriverKeys::CONTROL, 'c']);
+        });
+
+        Keyboard::macro('paste', function () {
+            $this->type([OperatingSystem::onMac() ? WebDriverKeys::COMMAND : WebDriverKeys::CONTROL, 'v']);
+        });
+
+        Keyboard::macro('copyAll', function () {
+            $this->selectAll();
+            $this->copy();
+        });
     }
 }
